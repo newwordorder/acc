5e3596d19cfea88046e76bfcf86b2cfca0407fa2
Nicholas MacArthur <n.j.macarthur@gmail.com>
2019-01-15 17:30:00 +1000

    end of day push

--- page-templates/blocks/feature-tiles.php
+++ page-templates/blocks/feature-tiles.php
@@ -3,58 +3,29 @@
 if( get_row_layout() == 'feature_tiles' ):
 
   $spaceBelow = get_sub_field('feature_tiles_space_below');
-  $type = get_sub_field('feature_tiles_type');
   $columns = get_sub_field('feature_tiles_columns_per_row');
-
+  $sectiontitle = get_sub_field('feature_tiles_title')
 ?>
 
 
   <?php if( have_rows('feature_tiles_column') ): ?>
     <div class="container space-below--<?php echo $spaceBelow ?>">
-      <div class="row justify-content-center">
+      <div class="row">
+      <div class="col-md-12"><h5><?echo $sectiontitle?></h5></div>
+
         <?php while( have_rows('feature_tiles_column') ): the_row();
-
+          $title = get_sub_field('title');
           $text = get_sub_field('text_block');
-          $image = get_sub_field('background_image');
-          $link = get_sub_field('link');
-
         ?>
-
         <div class="col-md-<?php echo $columns; ?> ">
-          <a href="<?php echo $link ?>">
-            <div class="feature-tile rounded feature-tile--<?php echo $type ?> hover-element">
-                <div class="hover-element__initial" >
-                  <?php
-                  $image = get_sub_field('background_image');
-
-                  if( !empty($image) ):
-
-                    // vars
-                    $url = $image['url'];
-                    $alt = $image['alt'];
-
-                    ?>
-                    <div class="background-image-holder" >
-                      <img src="<?php echo $url; ?>" alt="<?php echo $alt; ?>"/>
-                    </div>
-                  <?php endif; ?>
+            <div class="feature-tile ">
+                <div class="feature-tile__header">
+                  <?php echo $title ?>
                 </div>
                 <div class="feature-tile__content">
                   <?php echo $text ?>
-
-                  <?php get_template_part( 'page-templates/blocks/block-partials/buttons' ); ?>
-                  </div>
-                  <div class="hover-element__reveal" data-overlay="8">
-
                   </div>
               </div>
-            </a>
-
-
-
-
-
-
             </div>
         <?php endwhile; ?>
       </div>
