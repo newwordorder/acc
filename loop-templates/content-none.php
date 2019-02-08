d2641e6b1e926deefb4b7223d610f5aa76f69647
Elliot <elliot@newwordorder.com.au>
2019-02-08 10:31:47 +1000

    Search

--- loop-templates/content-none.php
+++ loop-templates/content-none.php
@@ -9,13 +9,11 @@
 
 ?>
 
-<section class="no-results not-found">
+<section class="no-results not-found pt-0">
 
-	<header class="page-header">
 
-		<h1 class="page-title"><?php esc_html_e( 'Nothing Found', 'understrap' ); ?></h1>
+		<h1><?php esc_html_e( 'Nothing Found', 'understrap' ); ?></h1>
 
-	</header><!-- .page-header -->
 
 	<div class="page-content">
 
@@ -30,7 +28,7 @@
 
 		<?php elseif ( is_search() ) : ?>
 
-			<p><?php esc_html_e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'understrap' ); ?></p>
+			<p class="mb-4"><?php esc_html_e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'understrap' ); ?></p>
 			<?php
 				get_search_form();
 		else : ?>
